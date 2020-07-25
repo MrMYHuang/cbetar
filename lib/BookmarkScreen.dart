5954fd4048b1f39b9e85176afdbbeff33bd4c96c
Meng-Yuan Huang <myh@live.com>
2020-07-25 21:33:02 +0800

    * Show message if no bookmark.

--- lib/BookmarkScreen.dart
+++ lib/BookmarkScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
+import 'Globals.dart';
 import 'Redux.dart';
 import 'WebViewScreen.dart';
 
@@ -21,31 +22,40 @@ class _BookmarkScreen extends State<BookmarkScreen> {
           appBar: AppBar(
             title: Text("書籤"),
           ),
-          body: ListView.separated(
-              separatorBuilder: (context, index) => Divider(
-                color: Colors.black,
-                thickness: 1,
-              ),
-              itemCount: vm.bookmarks.length,
-              itemBuilder: (BuildContext context, int index) {
-                // access element from list using index
-                // you can create and return a widget of your choice
-                final bookmark = vm.bookmarks[index];
-                return GestureDetector(
+          body: vm.bookmarks.length == 0
+              ? Center(
                   child: Text(
-                    "${bookmark.work.title}第${bookmark.work.juan}卷\n${bookmark.selectedText}",
-                    style: TextStyle(fontSize: 40),
+                    '無書籤',
+                    style: TextStyle(fontSize: fontSizeLarge),
                   ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                          pageBuilder: (context, animation1, animation2) =>
-                              WebViewScreen(work: vm.bookmarks[index].work, bookmarkUuid: vm.bookmarks[index].uuid)),
+                )
+              : ListView.separated(
+                  separatorBuilder: (context, index) => Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                  itemCount: vm.bookmarks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    // access element from list using index
+                    // you can create and return a widget of your choice
+                    final bookmark = vm.bookmarks[index];
+                    return GestureDetector(
+                      child: Text(
+                        "${bookmark.work.title}第${bookmark.work.juan}卷\n${bookmark.selectedText}",
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              pageBuilder: (context, animation1, animation2) =>
+                                  WebViewScreen(
+                                      work: vm.bookmarks[index].work,
+                                      bookmarkUuid: vm.bookmarks[index].uuid)),
+                        );
+                      },
                     );
-                  },
-                );
-              }));
+                  }));
     });
   }
 }
--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 double fontSizeNorm = 20;
+double fontSizeLarge = 40;
 String apiVersion = 'v1.2';
 String cbetaApiUrl = 'http://cbdata.dila.edu.tw/${apiVersion}';
